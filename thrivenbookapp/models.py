8d70d085a8b6f70780fb472a9b26650016ffb9c2
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-07-25 13:31:33 +0000

    Add: Create Constants

--- thrivenbookapp/models.py
+++ thrivenbookapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Constants
+GENRES = [
+    ('action', 'Action'),
+    ('comedy', 'Comedy'),
+    ('romance', 'Romance'),
+    ('horror', 'Horror'),
+    ('sci-fi', 'Science Fiction'),
+]
+
+AGE_RATINGS = [
+    ('14+', '14+'),
+    ('16+', '16+'),
+    ('18+', '18+'),
+]
+
+SKILL_LEVELS = [
+    ('beginner', 'Beginner'),
+    ('casual', 'Casual'),
+    ('professional', 'Professional'),
+]
+
+PRONOUNS = [
+    ('he', 'He'),
+    ('she', 'She'),
+    ('other', 'Other'),
+]
