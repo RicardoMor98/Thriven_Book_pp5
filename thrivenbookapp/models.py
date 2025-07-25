6aa56afb44aed2da21f372413b6f36971d459c34
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-07-25 13:32:20 +0000

    Add: Create user model

--- thrivenbookapp/models.py
+++ thrivenbookapp/models.py
@@ -27,3 +27,11 @@ PRONOUNS = [
     ('she', 'She'),
     ('other', 'Other'),
 ]
+
+# Create your models here.
+class User(AbstractUser):
+    pronoun = models.CharField(max_length=10, choices=PRONOUNS)
+    date_of_birth = models.DateField()
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    is_author = models.BooleanField(default=False)
+    skill_level = models.CharField(max_length=20, choices=SKILL_LEVELS, null=True, blank=True)
